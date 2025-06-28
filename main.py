1aa64a8f5a9434b1156fb32f0c979b202c3ff546
ERIQUE DUARTE SANTIAGO <73600631+eriqueduarte@users.noreply.github.com>
2025-06-27 21:27:44 -0300

    Update main.py
    Apagar os dados de token

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import pandas as pd
 from twilio.rest import Client
 
 # Your Account SID and Auth Token from console.twilio.com
-account_sid = "ACac21b7a42a731e1c3e9f84c96583f245"
-auth_token  = "6637e504d0ae0c7e9f3683d3ef121e20"
+account_sid = "AS###################"
+auth_token  = "#####################"
 client = Client(account_sid, auth_token)
 
 # Passo a passo para a solução
@@ -20,7 +20,7 @@ for mes in lista_meses:
         vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000,'Vendas'].values[0]
         print(f'No mês de {mes} alguém bateu a meta. Seguem as informações: Vendedor: {vendedor}, Vendas: {vendas} ')
         message = client.messages.create(
-            to="+5582996598985",
+            to="+55###$$$###",
             from_="+1 507 436 2604",
             body=f'No mês de {mes} alguém bateu a meta. Seguem as informações: Vendedor: {vendedor}, Vendas: {vendas} ')
         print(message.sid)
